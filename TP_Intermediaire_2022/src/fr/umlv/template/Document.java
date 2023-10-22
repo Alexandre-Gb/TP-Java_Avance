a5c91b368542f55c9cb2283368d84f2ce8e39e4f
Alexandre-Gb <giboz.alexandre@gmail.com>
2023-10-22 17:47:14 +0200

    Q3

--- TP_Intermediaire_2022/src/fr/umlv/template/Document.java
+++ TP_Intermediaire_2022/src/fr/umlv/template/Document.java
@@ -1,5 +1,6 @@
 package fr.umlv.template;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -28,6 +29,22 @@ public class Document {
       }
 
       return stringBuilder.append(fragmentsIterator.next()).toString();
+    }
+
+    public static Template of(String value) {
+      Objects.requireNonNull(value);
+
+      var template = new ArrayList<String>();
+      int start = 0;
+      for (int i = 0; i < value.length(); i++) {
+        if (value.charAt(i) == '@') {
+          template.add(value.substring(start, i));
+          start = i + 1;
+        }
+      }
+
+      template.add(value.substring(start));
+      return new Template(template);
     }
 
     @Override
