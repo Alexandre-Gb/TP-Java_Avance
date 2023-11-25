7bf3a87d119f3d750f608e3f69f5b0620b88d94f
Alexandre-Gb <giboz.alexandre@gmail.com>
2023-11-25 21:58:01 +0100

    Q1 and Q2

---
+++ TP_Session_2_2022/src/fr/uge/test/UnitTest.java
@@ -0,0 +1,43 @@
+package fr.uge.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class UnitTest {
+  private final HashMap<String, Runnable> tests = new HashMap<>();
+  public static void checkEquals(Object first, Object second) {
+    if (!Objects.equals(first, second)) {
+      throw new AssertionError(first + " is not equal to " + second);
+    }
+  }
+
+  public void test(String testName, Runnable runnable) {
+    Objects.requireNonNull(testName);
+    Objects.requireNonNull(runnable);
+    if (tests.containsKey(testName)) {
+      throw new IllegalStateException();
+    }
+
+    tests.put(testName, runnable);
+  }
+
+  public List<Error> runOnly(String testName) {
+    Objects.requireNonNull(testName);
+    if (!tests.containsKey(testName)) {
+      throw new IllegalStateException();
+    }
+
+    try {
+      tests.get(testName).run();
+    } catch (AssertionError e) {
+      return List.of(new Error(e.getMessage()));
+    }
+
+    return List.of();
+  }
+
+  public int testCount() {
+    return tests.size();
+  }
+}
